59c126639b3d4112a9b7d02b3dc51fcb21eef975
Martin <mcarstensen79@gmail.com>
2025-09-11 11:29:18 +0200

    Update quell.py

--- quell.py
+++ quell.py
@@ -703,7 +703,7 @@ if excel_file and key_file:
             st.download_button(
                 "Download HTML",
                 data=final_html.encode("utf-8"),
-                file_name="suche_tech_look.html",
+                file_name="suche.html",
                 mime="text/html",
                 type="primary"
             )
